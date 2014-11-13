99f3d001fb1c42791577c7f810efb1b5009814e8
Alexander Gorelov <gorelov@MacBook-Pro-Alexander.local>
2014-11-13 15:51:54 +0600

    change dir structure

---
+++ GrcsTorBundle.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of the Grcs package.
+ *
+ * (c) Alexander Gorelov <grac.ga@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Grcs\TorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class GrcsTorBundle
+ *
+ * @package Grcs\TorBundle
+ */
+class GrcsTorBundle extends Bundle
+{
+
+}
